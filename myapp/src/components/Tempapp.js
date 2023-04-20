90d4c6d2b131a51b6f04592276402ba28a250de6
anjalibvn <Anjali.patelbvn@gmail.com>
2023-04-20 22:14:13 +0530

    commited

--- myapp/src/components/Tempapp.js
+++ myapp/src/components/Tempapp.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./css/style.css";
 const Tempapp = () => {
-    const [city,setCity]=useState(null);
-    const [search,setSearch]=useState("Mumbai");
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
 
-    useEffect(()=>{
-        const fetchApi = async()=>{
-            const url=`https://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=d3c19dc091059ec87387ebd1f028c24f`
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=d3c19dc091059ec87387ebd1f028c24f`
             const response = await fetch(url);
             const resJson = await response.json();
             //console.log(resJson);
@@ -14,35 +14,42 @@ const Tempapp = () => {
         }
         fetchApi();
 
-    },[search])
+    }, [search])
     return (
         <>
             <div className="box">
                 <div className="inputData">
                     <input type="search"
                         className="inputField"
+                        value={search}
                         onChange={
                             (event) => {
-                                   setSearch(event.target.value)
+                                setSearch(event.target.value)
                             }
                         }
                     ></input>
                 </div>
-          
-            <div className="info">
-                <h2 className="location">
-                    <i className="fas fa-street-view"></i>
-                    {search}</h2>
-                <h1 className="temp">
-                 {city.temp}
-                </h1>
-                <h3 className="tempmin_max">
-                     
-                </h3>
-            </div>
-            <div className="wave-one"></div>
-            <div className="wave-two"></div>
-            <div className="wave-three"></div>
+                {
+                    !city ? (<p className="errorMsg">No data found</p>) :
+                        (<div>
+                            <div className="info">
+                                <h2 className="location">
+                                    <i className="fas fa-street-view"></i>
+                                    {search}</h2>
+                                <h1 className="temp">
+                                    {city.temp}
+                                </h1>
+                                <h3 className="tempmin_max">
+                                    Min:{city.temp_min}||Max:{city.temp_max}
+                                </h3>
+                            </div>
+                            <div className="wave-one"></div>
+                            <div className="wave-two"></div>
+                            <div className="wave-three"></div>
+                        </div>
+                        )
+                }
+
             </div>
         </>
     )
